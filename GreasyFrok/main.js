25c906ef042ecbc59b9018afe97e7316b2253158
Jzianchen <2645825504@qq.com>
2024-09-20 22:48:32 +0800

    NewScript

---
+++ GreasyFrok/main.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         HelloWorld
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-20
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.tampermonkey.net/scripts.php?ext=dhdg&updated=true&version=5.2.3
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @license    GPL-3.0-only
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("HelloWorld")
+    // Your code here...
+})();
