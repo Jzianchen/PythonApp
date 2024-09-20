1b7bbf522870f9869742b9ba0cbdba99624859c6
Jzianchen <2645825504@qq.com>
2024-09-20 22:50:58 +0800

    change description

--- GreasyFrok/main.js
+++ GreasyFrok/main.js
@@ -2,7 +2,7 @@
 // @name         HelloWorld
 // @namespace    http://tampermonkey.net/
 // @version      2024-09-20
-// @description  try to take over the world!
+// @description  New a new project
 // @author       You
 // @match        https://www.tampermonkey.net/scripts.php?ext=dhdg&updated=true&version=5.2.3
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
